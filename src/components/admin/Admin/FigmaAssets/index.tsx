83031dae9e4d6e79be05ae9025b441730e55682b
funny2code <dejanbuk.dev@hotmail.com>
2023-11-14 15:35:19 +0100

    added assets delete ui using ag grid

--- src/components/admin/Admin/FigmaAssets/index.tsx
+++ src/components/admin/Admin/FigmaAssets/index.tsx
@@ -6,6 +6,7 @@ import { useSession } from "../../../auth/useSession";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { V3FigmaProjectsService } from "../../../../client/index.ts";
 import { useInView } from "react-intersection-observer";
+import { GridApi } from "ag-grid-community";
 
 const FigmaAssets = () => {
   const { getSession } = useSession();
@@ -15,6 +16,14 @@ const FigmaAssets = () => {
   const { ref, inView } = useInView();
   const pageSize = 10;
   const [columnDefs, setColumnDefs] = useState([
+    {
+      headerName: '',
+      checkboxSelection: true,
+      headerCheckboxSelection: true,
+      pinned: 'left',
+      width: 50,
+      field: 'checkboxBtn'
+    },
     { field: "id", filter: true },
     { field: "description", filter: true },
     { field: "type" },
@@ -40,7 +49,6 @@ const FigmaAssets = () => {
     async ({ pageParam = 0 }) => {
       const tokens = await getSession();
       const data = await V3FigmaProjectsService.readAllFigmaColors(pageParam, pageSize);
-      
       setRowData(data.color_palettes);
       setDepleted(data.results.length < pageSize);
       return { data: data.results, previousId: pageParam - pageSize, nextId: pageParam + pageSize };
@@ -73,7 +81,15 @@ const FigmaAssets = () => {
   return (
     <>
       {/* Example using Grid's API */}
-      <button onClick={buttonListener}>Push Me</button>
+      <div style={{display: "flex", flexDirection: "row", marginBottom: '1px'}}>
+        <button style={{margin: '0px', width: 'fit-content'}} onClick={buttonListener}>Push Me</button>
+        <button style={{margin: '0px', width: 'fit-content'}} onClick={ _ => {
+          const selectedRows = gridRef.current.api.getSeletectedRows()
+          gridRef.current.api.applyTransaction({ remove: selectedRows })
+        }} >
+          Delete selected Rows
+        </button>
+      </div>
 
       {/* On div wrapping Grid a) specify theme CSS Class Class and b) sets Grid size */}
       <div className="ag-theme-alpine" style={{ width: 'auto', height: '100%' }}>
